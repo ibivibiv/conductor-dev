452e21e949e2704cf415a1a8f72dfcb46807002c
Harold Hannon <haroldhannon@hotmail.com>
2018-12-02 09:19:04 -0600

    Fixing Host Token Hack
    This is really pretty hacky to begin with.  Jedis would only find one host before.  This assumes only one token so no sharding but I will fix that later.  I should be able to build a Kubernetes version that can pull the token for a given host.

--- redis-persistence/src/main/java/com/netflix/conductor/jedis/TokenMapSupplierProvider.java
+++ redis-persistence/src/main/java/com/netflix/conductor/jedis/TokenMapSupplierProvider.java
@@ -8,6 +8,7 @@ import com.netflix.dyno.connectionpool.TokenMapSupplier;
 import com.netflix.dyno.connectionpool.impl.lb.HostToken;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -28,16 +29,27 @@ public class TokenMapSupplierProvider implements Provider<TokenMapSupplier> {
 
             // FIXME This isn't particularly safe, but it is equivalent to the existing code.
             // FIXME It seems like we should be supply tokens for more than one host?
-            HostToken token = new HostToken(1L, Lists.newArrayList(hostSupplier.getHosts()).get(0));
+            //HostToken token = new HostToken(1L, Lists.newArrayList(hostSupplier.getHosts()).get(0));
+            
+            
 
             @Override
             public List<HostToken> getTokens(Set<Host> activeHosts) {
-                return Arrays.asList(token);
+                
+                 ArrayList<HostToken> tokenList = new ArrayList<HostToken>();
+                
+                for (int i = 0; i < activeHosts.size(); i++) {
+                 Host host = activeHosts.get(i);
+                 tokenList.add(new HostToken(4294967295L, host));
+	             }
+                
+                
+                return tokenList;
             }
 
             @Override
             public HostToken getTokenForHost(Host host, Set<Host> activeHosts) {
-                return token;
+                return new HostToken(4294967295L, host);
             }
         };
     }
