717fbe3f7768b57338301056e366c6fe6b76026a
Harold Hannon <haroldhannon@hotmail.com>
2018-12-18 01:26:59 -0600

    Update IndexEventProducer.java

--- es5-persistence/src/main/java/com/netflix/conductor/dao/es5/index/eventing/IndexEventProducer.java
+++ es5-persistence/src/main/java/com/netflix/conductor/dao/es5/index/eventing/IndexEventProducer.java
@@ -3,6 +3,7 @@ package com.netflix.conductor.dao.es5.index.eventing;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.conductor.common.metadata.tasks.Task;
 import com.netflix.conductor.common.run.Workflow;
 
@@ -15,12 +16,15 @@ public class IndexEventProducer extends Producer {
 	}
 
 	public void sendWorkflow(String indexName, String id, Workflow workflow) throws IOException {
-		String message = "";
+
+		ObjectMapper mapper = new ObjectMapper();
+		String message = mapper.writeValueAsString(workflow);
 		channel.basicPublish(exchange, routingKey, null, message.getBytes());
 	}
 
 	public void sendTask(String indexName, String id, Task task) throws IOException {
-		String message = "";
+		ObjectMapper mapper = new ObjectMapper();
+		String message = mapper.writeValueAsString(task);
 		channel.basicPublish(exchange, routingKey, null, message.getBytes());
 	}
 
